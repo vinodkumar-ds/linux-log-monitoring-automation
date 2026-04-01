f4239c00d46c1c4fae069d8ece105f9ce4e27e3a
Vinod Kumar <vinod.ds.kumar@gmail.com>
2026-04-01 21:46:42 +0530

    Added CPU monitoring script alert status

--- cpu_monitor.sh
+++ cpu_monitor.sh
@@ -9,3 +9,16 @@ CPU=$(echo "100 - $usage" | bc)
 echo "CPU idle : $usage %"
 echo "CPU used : $CPU %"
 
+if (( $(echo "$CPU > 90" | bc -l) ))
+then 
+	echo "STATUS : CRITICAL"
+elif (( $(echo "$CPU > 75" | bc -l)))
+then 
+	echo " STATUS : HIGH"
+elif (( $(echo "$CPU > 60" | bc -l)))
+then
+	echo " STATUS : WARNING "
+else
+	echo " STATUS : OK"
+fi
+
